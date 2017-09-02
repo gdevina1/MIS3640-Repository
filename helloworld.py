77e8d81f778bbd152700c16ed3cc567ef3a4b242
gdevina1 <31513644+gdevina1@users.noreply.github.com>
2017-09-02 04:35:50 -0400

    in class 1, exercise 1

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,23 @@ print("Hello, World")
 print("Hello, Gianca")
 
 print("This is fun")
+
+print("In a print statement, what happens if you leave out one of the parentheses, or both?")
+
+print("shows a syntax error message")
+
+print("If you are trying to print a string, what happens if you leave out one of the quotation marks, or both?")
+
+print("shows a syntax error message")
+
+print ("You can use a minus sign to make a negative number like -2. What happens if you put a plus sign before a number? What about 2++2?")
+
+print("it reads it as a regular plus sign")
+
+print("In math notation, leading zeros are ok, as in 02. What happens if you try this in Python?")
+
+print("syntax error for invalid token")
+
+print("What happens if you have two values with no operator between them?")
+
+print("syntax error for invalid syntax")
