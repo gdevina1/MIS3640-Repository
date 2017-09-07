9d286efd8a0ae598d88c5c79efb6e4d9acabde40
gdevina1 <31513644+gdevina1@users.noreply.github.com>
2017-09-06 22:01:59 -0400

    added: Class Session 2, Exercise 1

--- calc.py
+++ calc.py
@@ -8,7 +8,7 @@ print(42*60+42)
 print("How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile.")
 print(10/1.61)
 print("If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per mile in minutes and seconds)? What is your average speed in miles per hour?")
-print(6.211180124223602/2562*60)
+print(6.211180124223602/2562*60) 
 print("disclaimer: Math is not really my strong suit.")
 
 print("")
