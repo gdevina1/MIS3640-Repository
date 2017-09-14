aca23dc203e6df8766e370d125afd9f158fab2c1
gdevina1 <31513644+gdevina1@users.noreply.github.com>
2017-09-13 23:05:21 -0400

    quadratic formula solver

---
+++ Session4Exercise.py
@@ -0,0 +1,17 @@
+print("Session 4 - Functions Exercise")
+
+import math
+
+def quadratic(a, b, c):
+        a = float(a)
+        b = float(b)
+        c = float(c)
+        quad_calc_plus = (-b + math.sqrt((b**2)-4*a*c)/2*a)
+        quad_calc_min = (-b - math.sqrt((b**2)-4*a*c)/2*a)
+        print("The solution(s) to the quadratic equation:" , quad_calc_plus, ",", quad_calc_min)
+
+quadratic(8,-8,0)
+
+    
+    
+
