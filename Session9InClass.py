c4b8d5d59f31d57414c60b7f22b46080d532a66c
gdevina1 <31513644+gdevina1@users.noreply.github.com>
2017-10-04 22:14:36 -0400

    Session 9 Exercises

---
+++ Session9InClass.py
@@ -0,0 +1,15 @@
+print("Session 9- In Class")
+
+url= 'http://www.babson.edu'
+print(url.startswith('htt'))
+print(url.endswith('.edu'))
+print(url.startswith('hhe'))
+
+s = 'jalapeno'
+print(s)
+s = 'jalape\u00f1o'
+print(s)
+
+words = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
+#use ord chr
+
